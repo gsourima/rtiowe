7518123ba7a8338ff3b67d2577132ee1aa5f501d
Gael Sourimant <gael.sourimant@technicolor.com>
2018-04-04 12:56:12 +0200

    Chapter 3

--- Cpp/include/vec3.h
+++ Cpp/include/vec3.h
@@ -11,6 +11,7 @@ class vec3  {
 public:
     vec3() {}
     vec3(float e0, float e1, float e2) { e[0] = e0; e[1] = e1; e[2] = e2; }
+    vec3(float eX) { e[0] = eX; e[1] = eX; e[2] = eX; }
     inline float x() const { return e[0]; }
     inline float y() const { return e[1]; }
     inline float z() const { return e[2]; }
--- Cpp/src/main.cpp
+++ Cpp/src/main.cpp
@@ -1,7 +1,14 @@
-#include "vec3.h"
+#include "ray.h"
 
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image_write.h"
+
+vec3 ray_color( const ray& r )
+{
+    vec3 unit_dir = unit_vector( r.direction() );
+    float t = 0.5f * unit_dir.y() + 0.5f;
+    return (1.0f-t)*vec3(1.0) + t*vec3(0.5f, 0.7f, 1.0f);
+}
 
 int main()
 {
@@ -11,11 +18,21 @@ int main()
     char* img = new char[ w * h * 3 ];
     int k = 0;
 
+    vec3 corner_ll( -2, -1, -1 );
+    vec3 horiz( 4, 0, 0 );
+    vec3 vert( 0, 2, 0 );
+    vec3 orig( 0 );
+
     for ( int j = h-1; j >= 0; j-- )
     {
         for ( int i = 0; i < w; i++ )
         {
-            vec3 col( float(i) / float(w), float(j) / float(h), 0.2f );
+            float u = float(i) / float(w);
+            float v = float(j) / float(h);
+
+            ray r( orig, corner_ll + u*horiz + v * vert );
+            
+            vec3 col = ray_color( r );
 
             img[k++] = char( 255.99f*col[0] );
             img[k++] = char( 255.99f*col[1] );
