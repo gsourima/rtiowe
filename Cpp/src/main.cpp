e9aa14ed7bc00433a790fdef3e60c0920a3756e3
Gael Sourimant <gael.sourimant@technicolor.com>
2018-04-05 09:08:42 +0200

    Chapter 7
    Diffuse materials

--- Cpp/src/main.cpp
+++ Cpp/src/main.cpp
@@ -1,6 +1,7 @@
 #include <climits>
 #include <cstdlib>
 #include <ctime>
+#include <cstdio>
 
 #include "camera.h"
 #include "hitable_list.h"
@@ -9,19 +10,36 @@
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image_write.h"
 
+float randf() { return rand() / float( RAND_MAX ); }
+
+vec3 random_in_unit_sphere()
+{
+    vec3 p;
+    do { p = 2 * vec3( randf(), randf(), randf() ) - 1; }
+    while ( dot(p,p) >= 1 );
+    return p;
+}
+
 vec3 ray_color( const ray& r, hitable* scene )
 {
     hit_record rec;
 
     const int N = 2;
 
-    if ( scene->hit( r, 0, std::numeric_limits<float>::max(), rec ) )
+#define TMIN_EPS 0.001f
+
+    if ( scene->hit( r, TMIN_EPS, std::numeric_limits<float>::max(), rec ) )
     {        
+        // Outputting diffuse reflection
+        vec3 target = rec.P + rec.N + random_in_unit_sphere();
+        return 0.5f * ray_color( ray( rec.P, target-rec.P ), scene );
+
         // Outputting ids as colors
         // int id = 1 + rec.id;
         // return vec3( id/float(N), (N-id)/float(N), (id+N)/float(2*N) );
         
-        return 0.5f * rec.N + 0.5f;
+        // Outputting normal
+        // return 0.5f * rec.N + 0.5f;
     }
 
     vec3 unit_dir = unit_vector( r.direction() );
@@ -29,17 +47,16 @@ vec3 ray_color( const ray& r, hitable* scene )
     return (1.0f-t)*vec3(1.0) + t*vec3(0.5f, 0.7f, 1.0f);
 }
 
-float randf() { return rand() / float( RAND_MAX ); }
-
 int main()
 {
     std::srand( (unsigned int) std::time(NULL));
 
     //int w = 240;
     //int h = 135;
-    int w = 960;
-    int h = 540;
-    int s = 32;
+    const int w = 960;
+    const int h = 540;
+    const int s = 32;
+    const int pixels = w*h;
 
     char* img = new char[ w * h * 3 ];
     int p = 0;
@@ -67,11 +84,20 @@ int main()
             }
             col /= float(s);
 
-            img[p++] = char( 255.99f*col[0] );
-            img[p++] = char( 255.99f*col[1] );
-            img[p++] = char( 255.99f*col[2] );
+            // Includes Gamma correction
+            img[p++] = char( 255.99f*pow(col[0],0.4545f) );
+            img[p++] = char( 255.99f*pow(col[1],0.4545f) );
+            img[p++] = char( 255.99f*pow(col[2],0.4545f) );
+        }
+
+        if ( (j%4) == 0 )
+        {
+            float progress = 100 * p / float( 3 * pixels );
+
+            printf( "Rendering in progress... [ %5.1f%% ]\r", progress );
         }
     }
+    printf( "\nDone!" );
 
     stbi_write_png( "out.png", w, h, 3, img, 0 );
 }
